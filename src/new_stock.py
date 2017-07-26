39a6161becee3b16e0272f393c81b07c129357e1
Maciej Bogusz <maciekbogusz@yahoo.com>
2017-07-26 15:43:24 +0200

    start of project

---
+++ src/new_stock.py
@@ -0,0 +1,9 @@
+'''
+Created on 26.07.2017
+
+@author: mcbg
+'''
+from stock import Stock
+
+newStock = Stock('PXM')
+print(newStock.name)
--- src/stock.py
+++ src/stock.py
@@ -3,3 +3,9 @@ Created on 26.07.2017
 
 @author: mcbg
 '''
+class Stock(object):
+    def __init__(self, name):
+        self.name = name
+        
+    def getName(self):
+        return self.name
